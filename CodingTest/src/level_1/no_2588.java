f1e564794533e491bc9eecbbaaf8880429842474
Roar Joho <itsjiheemin@gmail.com>
2021-12-31 14:58:08 +0900

    백준 레벨1

---
+++ CodingTest/src/level_1/no_2588.java
@@ -0,0 +1,19 @@
+package level_1;
+
+import java.util.Scanner;
+
+public class no_2588 {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+
+	
+	System.out.println(a*(b%10));
+	System.out.println(a*((b%100)/10));
+	System.out.println(a*(b/100));
+	System.out.println(a*b);
+	
+}
+}
